3edc53573099b09c3572d2c4beaa6bb46f5691b1
MTLantoine <mortelier.antoine@hotmail.fr>
2020-04-23 15:20:06 +0200

    refact passion model and service

--- src/app/assets/models/Passion.model.ts
+++ src/app/assets/models/Passion.model.ts
@@ -1,7 +1,13 @@
+import { Picture } from "./Picture.model";
+
 export class Passion {
   constructor(public title: string, public passion: OnePassion[]) {}
 }
 
 export class OnePassion {
-  constructor(public title: string, public example?: string[]) {}
+  constructor(public title: string, public example?: OneExample[]) {}
+}
+
+export class OneExample {
+  constructor(public title: string, public picto?: Picture) {}
 }
--- src/app/assets/services/passion.service.ts
+++ src/app/assets/services/passion.service.ts
@@ -2,12 +2,47 @@ import * as models from "../models";
 
 export class PassionService {
   passion = new models.Passion("Passions", [
+    // Sport
     new models.OnePassion("Sport", [
-      "Natation en club",
-      "Musculation",
-      "Cyclisme",
+      new models.OneExample(
+        "Natation en club",
+        new models.Picture("./assets/pic/svg/swim.svg", "Natation en club")
+      ),
+      new models.OneExample(
+        "Musculation",
+        new models.Picture("./assets/pic/svg/gym.svg", "Musculation")
+      ),
+      new models.OneExample(
+        "Cyclisme",
+        new models.Picture("./assets/pic/svg/bycicle.svg", "Cyclisme")
+      ),
     ]),
-    new models.OnePassion("Musique", ["Piano", "Guitare"]),
-    new models.OnePassion("Informatique", ["Jeux vidéo", "Programmation"]),
+
+    // Musique
+    new models.OnePassion("Musique", [
+      new models.OneExample(
+        "Piano",
+        new models.Picture("./assets/pic/svg/piano.svg", "Piano")
+      ),
+      new models.OneExample(
+        "Guitare",
+        new models.Picture("./assets/pic/svg/music.svg", "Guitare")
+      ),
+    ]),
+
+    // Informatique
+    new models.OnePassion("Informatique", [
+      new models.OneExample(
+        "Jeux vidéo",
+        new models.Picture("./assets/pic/svg/game-controller.svg", "Jeux vidéo")
+      ),
+      new models.OneExample(
+        "Programmation",
+        new models.Picture(
+          "./assets/pic/svg/web-development.svg",
+          "Programmation"
+        )
+      ),
+    ]),
   ]);
 }
