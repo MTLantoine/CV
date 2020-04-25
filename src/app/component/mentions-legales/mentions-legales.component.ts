495444036fd61ca78a3dfa6bc4a09cd5d29831b1
MTLantoine <mortelier.antoine@hotmail.fr>
2020-04-25 22:24:58 +0200

    added mentions legales

--- src/app/component/mentions-legales/mentions-legales.component.ts
+++ src/app/component/mentions-legales/mentions-legales.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-mentions-legales',
-  templateUrl: './mentions-legales.component.html',
-  styleUrls: ['./mentions-legales.component.scss']
+  selector: "app-mentions-legales",
+  templateUrl: "./mentions-legales.component.html",
+  styleUrls: ["./mentions-legales.component.scss"],
 })
 export class MentionsLegalesComponent implements OnInit {
+  frText: string;
+  enText: string;
+  frDate: string;
+  enDate: string;
+  date: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.initMentionsLegales();
   }
 
+  initMentionsLegales() {
+    this.frText =
+      "Ce site a été créé sur AngularJS par Antoine Mortelier, et appartient à Antoine Mortelier.";
+    this.frDate = "Dernière date de mise à jour :";
+    this.enText =
+      "This website was created on AngularJS by Antoine Mortelier, and belongs to Antoine Mortelier.";
+    this.enDate = "Last update :";
+    this.date = "25/04/2020 -- 21:13";
+  }
 }
