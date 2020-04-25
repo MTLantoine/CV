1a4388047e94f7b8be2fb79e190844dcfb92f9d8
MTLantoine <mortelier.antoine@hotmail.fr>
2020-04-25 20:56:31 +0200

    fixed the height of 404 page

--- src/app/component/not-found/not-found.component.ts
+++ src/app/component/not-found/not-found.component.ts
@@ -4,9 +4,20 @@ import { Component, OnInit } from "@angular/core";
   selector: "app-not-found",
   templateUrl: "./not-found.component.html",
   styleUrls: ["./not-found.component.scss"],
+  host: {
+    "(window:resize)": "onResize($event)",
+  },
 })
 export class NotFoundComponent implements OnInit {
+  windowHeight: number;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.windowHeight = window.innerHeight - 40;
+  }
+
+  onResize(event) {
+    this.windowHeight = event.target.innerHeight - 40;
+  }
 }
