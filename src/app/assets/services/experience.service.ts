c850c69b4ca0d4815bfcd732d273968f148d9121
MTLantoine <mortelier.antoine@hotmail.fr>
2020-04-25 23:21:25 +0200

    changed experience service

--- src/app/assets/services/experience.service.ts
+++ src/app/assets/services/experience.service.ts
@@ -4,27 +4,27 @@ export class ExperienceService {
   experience = new models.Experience("Expériences", [
     new models.OneExperience(
       new models.Date("2019", "Avril - Août"),
-      "Yoozly (Stage)",
+      "Développeur Front-End",
       "Conception de sites Web sous AngularJS",
-      "Développeur Front-End"
+      "Yoozly (Stage)"
     ),
     new models.OneExperience(
       new models.Date("2018", "Juillet - Août"),
-      "SDIS 2B (Service Départemental d'Incendie et de Secours)",
+      "Nageur sauveteur qualifié - Sapeur pompier volontaire",
       "Surveillance et sauvetage en zone de baignade et préventions sur la plage comme dans l'eau",
-      "Nageur sauveteur qualifié - Sapeur pompier volontaire"
+      "SDIS 2B (Service Départemental d'Incendie et de Secours)"
     ),
     new models.OneExperience(
       new models.Date("2017", "Juillet - Août"),
-      "SNSM (Société Nationale de Sauvetage en Mer)",
+      "Nageur sauveteur qualifié",
       "Surveillance et sauvetage en zone de baignade et préventions sur la plage comme dans l'eau",
-      "Nageur sauveteur qualifié"
+      "SNSM (Société Nationale de Sauvetage en Mer)"
     ),
     new models.OneExperience(
       new models.Date("2018 - 2020"),
-      "Deliveroo",
+      "Coursier à vélo",
       "Livraison de repas à domicile",
-      "Coursier à vélo"
+      "Deliveroo"
     ),
   ]);
 }
