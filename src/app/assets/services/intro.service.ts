28b012a52e60fcc17d8b7c36d0bde366621730ab
MTLantoine <mortelier.antoine@hotmail.fr>
2020-04-25 20:06:15 +0200

    modified the intro

--- src/app/assets/services/intro.service.ts
+++ src/app/assets/services/intro.service.ts
@@ -6,10 +6,10 @@ export class IntroService {
       "Diplômé d'une licence Informatique, je suis activement à la recherche d'une alternance pour les deux années à suivre (2020 - 2022)."
     ),
     new models.OneText(
-      "Je suis passionné d'informatique depuis mes années lycée, et compte bien faire de ma passion mon métier."
+      "Je suis passionné d'informatique depuis mes années lycée, créer de nouveaux programmes, surmonter des difficultés, sont vite devenu des tâches que j'aimais réaliser."
     ),
     new models.OneText(
-      "J'ai forgé mon expérience en réalisant des projets qui m'ont tous fait mieux connaître la technologie utilisée, mais j'ai aussi développé une fascination pour le développement de sites web lors de mon premier stage en entreprise."
+      "J'ai forgé mon expérience en réalisant des projets qui m'ont permis de me varier au niveau des technologies utilisées, j'ai aussi développé une fascination pour le développement de sites web lors de mon premier stage en entreprise."
     ),
     new models.OneText(
       "Motivé, dynamique, passionné, je suis toujours d'attaque pour trouver de nouvelles solutions à un problème."
