2e5789063eab48ad85d9457bb2172bdaf6f52f18
MTLantoine <mortelier.antoine@hotmail.fr>
2020-04-26 20:17:16 +0200

    new intro + dploy

--- src/app/assets/services/intro.service.ts
+++ src/app/assets/services/intro.service.ts
@@ -6,25 +6,28 @@ export class IntroService {
       "Diplômé d'une licence Informatique, je suis activement à la recherche d'une alternance pour les deux années à suivre (2020 - 2022)."
     ),
     new models.OneText(
-      "Je suis passionné d'informatique depuis mes années lycée, créer de nouveaux programmes, surmonter des difficultés, sont vite devenu des tâches que j'aimais réaliser."
+      "Passionné d'informatique, j’aime particulièrement créer de nouveaux programmes, surmonter des difficultés, sont vite devenu des tâches que j'aime réaliser."
     ),
     new models.OneText(
-      "J'ai forgé mon expérience en réalisant des projets qui m'ont permis de me varier au niveau des technologies utilisées, j'ai aussi développé une fascination pour le développement de sites web lors de mon premier stage en entreprise."
+      "Fort d’une expérience dans une entreprise spécialisée dans le développement de site internet, j’ai pu réaliser des projets utilisant différentes technologies."
     ),
     new models.OneText(
-      "Motivé, dynamique, passionné, je suis toujours d'attaque pour trouver de nouvelles solutions à un problème."
+      "Cet apprentissage a consolidé ma fascination pour le développement de sites web."
+    ),
+    new models.OneText(
+      "Motivé, rigoureux et surtout passionné, je cherche toujours à me dépasser."
     ),
     new models.OneText(
       "J'aime quand le travail est soigneusement réalisé, et quand le travail réalisé est à la pointe de la technologie."
     ),
     new models.OneText(
-      "Je cherche toujours à aller plus loin, à avancer en équipe, de manière organisée."
+      "De plus, mes différentes expériences professionnelles m’ont permis d’acquérir le sens des responsabilités et l’esprit d’équipe."
     ),
     new models.OneText(
       "C'est très important pour moi de postuler dans un secteur qui va me permettre d'exprimer ma motivation et de donner satisfaction dans le travail que je vais avoir la chance d'effectuer."
     ),
     new models.OneText(
-      "Je suis persuadé que mes diverses compétences peuvent vous intéresser, je peux ensuite vous certifier que c'est avec beaucoup de dynamisme et de passion que je vais occuper le poste que vous allez me confier."
+      "Grâce à ma polyvalence et à mes connaissances, je pense pouvoir être un élément complémentaire important au sein de votre équipe."
     ),
   ]);
 }
