596116a0ef2bb0353ec302a2e966346e56307eae
Antoine <mortelier.antoine@hotmail.fr>
2020-05-06 20:52:59 +0200

    added formation service

--- src/app/assets/services/formation.service.ts
+++ src/app/assets/services/formation.service.ts
@@ -13,13 +13,18 @@ export class FormationService {
       "Sauveteur qualifié aux interventions sur une personne en difficulté dans l'eau"
     ),
     new models.OneExperience(
-      new models.Date("2016"),
-      "Premiers Secours en Équipe 1 (PSE1)",
-      "Secourisme qualifié à l'aide des personnes en danger et premiers secours"
+      new models.Date("2019"),
+      "AngularJS",
+      "Framework JavaScript qui étend le HTML pour le rendre dynamique, et permet de développer ses propres balises et attributs HTML"
+    ),
+    new models.OneExperience(
+      new models.Date("2017"),
+      "Git",
+      "Logiciel de gestion de versions décentralisé"
     ),
     new models.OneExperience(
       new models.Date("2016"),
-      "Premiers Secours en Équipe 2 (PSE2)",
+      "Premiers Secours en Équipe 1 et 2 (PSE1 et PSE2)",
       "Secourisme qualifié à l'aide des personnes en danger et premiers secours"
     ),
   ]);
